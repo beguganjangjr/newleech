9321213d2fc50b4083b4c8ee76c118dd96ab8faa
begulatuk <65555163+begulatuk@users.noreply.github.com>
2021-04-23 13:25:59 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-
+wget -q https://github.com/xinxin8816/heroku-aria2c-21vianet/raw/master/rclone.zip
+unzip -qj rclone.zip -d /usr/bin
+rm -rf rclone.zip
+chmod 777 /usr/bin/rclone
 
 # Install aria2c static binary
 wget -q https://github.com/P3TERX/aria2-builder/releases/download/1.35.0_2020.09.04/aria2-1.35.0-static-linux-amd64.tar.gz
