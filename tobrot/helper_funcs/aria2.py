cc073c32e05b56f42ad2a5a2c3a1590985f62c9e
begulatuk <65555163+begulatuk@users.noreply.github.com>
2021-05-01 21:58:22 +0700

    Update aria2.py

--- tobrot/helper_funcs/aria2.py
+++ tobrot/helper_funcs/aria2.py
@@ -3,42 +3,36 @@ import aria2p
 import logging
 
 LOGGER = logging.getLogger(__name__)
+__config = {
+    "dir" : "downloads",
+    "daemon" : "true",
+    "max-connection-per-server" : "16",
+    "max-overall-upload-limit" : "1K",
+    "max-concurrent-downloads" : "3",
+    "bt-max-peers" : "0",
+    "rpc-listen-all" : "false",
+    "rpc-listen-port": "6800",
+    "seed-ratio" : "0",
+    "seed-time" : "0.01"
+}
 
-class aria2(aria2p.API):
-    __api =  None
-    __config = {
-        "dir" : "downloads",
-        "daemon" : "true",
-        "max-connection-per-server" : "16",
-        "max-overall-upload-limit" : "1K",
-        "max-concurrent-downloads" : "3",
-        "bt-max-peers" : "0",
-        "rpc-listen-all" : "false",
-        "rpc-listen-port": "6800",
-        "seed-ratio" : "0",
-        "seed-time" : "0.01"
-    }
-    __process = None
-
-    def __init__(self, config={}):
-        self.__config.update(config)
 
 async def aria_start():
-    cmd = []
-    for key in self.__config:
-        cmd.append(f"--{key}={self.__config[key]}")
-        LOGGER.info(cmd)
-        process = await asyncio.create_subprocess_exec(
-            *cmd,
-            stdout=asyncio.subprocess.PIPE,
-            stderr=asyncio.subprocess.PIPE
+    cmd = ["aria2c",
+          "--enable-rpc"]
+    cmd.append(f"--{__config})
+    LOGGER.info(cmd)
+    process = await asyncio.create_subprocess_exec(
+        *cmd,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE
+    )
+    stdout, stderr = await process.communicate()
+    LOGGER.info(stderr or stdout)
+    aria2 = aria2p.API(
+        aria2p.Client(
+            host="http://localhost",
+            port=6800
         )
-        stdout, stderr = await self.__process.communicate()
-        LOGGER.info(stderr or stdout)
-        aria2 = aria2p.API(
-            aria2p.Client(
-                host="http://localhost",
-                port=int(self.__config['rpc-listen-port'])
-            )
-        )
-        return aria2
+    )
+    return aria2
