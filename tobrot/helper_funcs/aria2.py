5b7674c12cbde66ddfcac9b9555397ed26a15f74
begulatuk <65555163+begulatuk@users.noreply.github.com>
2021-05-02 12:26:58 +0700

    Update aria2.py

--- tobrot/helper_funcs/aria2.py
+++ tobrot/helper_funcs/aria2.py
@@ -8,14 +8,51 @@ from pathlib import Path
 from tobrot import ARIA_CONF
 LOGGER = logging.getLogger(__name__)
 
-trackers_list = get(
-    "https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_best.txt"
-).text.replace("\n\n", ",")
-trackers = f"[{trackers_list}]"
+resp = requests.get('https://trackerslist.com/best_aria2.txt')
+conf = None
 xyza = f"{ARIA_CONF}"
 
-LOGGER.info(xyza)
 
+class aria2(aria2p.API):
+    __api =  None
+    __config = {
+        "dir" : "downloads",
+        "daemon" : "true",
+        "max-connection-per-server" : "10",
+        "rpc-listen-all" : "false",
+        "rpc-listen-port": "6800",
+        "seed-ratio" : "0",
+        "seed-time" : "0"
+    }
+    __process = None
+
+    def __init__(self, config={}):
+        self.__config.update(config)
+
+    async def init_start(self):
+        if not self.__process:
+            cmd = [
+                "aria2c",
+                "--enable-rpc"
+            ]
+            for key in self.__config:
+                cmd.append(f"--{key}={self.__config[key]}")
+            LOGGER.info(cmd)
+            self.__process = await asyncio.create_subprocess_exec(
+                *cmd,
+                stdout=asyncio.subprocess.PIPE,
+                stderr=asyncio.subprocess.PIPE
+            )
+            stdout, stderr = await self.__process.communicate()
+            LOGGER.info(stderr or stdout)
+        if not self.__api:
+            self.__api = aria2p.API(
+                aria2p.Client(
+                    host="http://localhost",
+                    port=int(self.__config['rpc-listen-port'])
+                )
+            )
+    
 async def aria_start():
     cmd = [
         "aria2c",
@@ -27,15 +64,19 @@ async def aria_start():
     cmd.append("--rpc-listen-all=false")
     cmd.append("--rpc-listen-port=6800")
     cmd.append("--seed-time=0")
-    cmd.append(f"--bt-tracker={trackers}")
+    #cmd.append(f"--bt-tracker={trackers}")
     if not os.path.exists("apic.conf"):
         with open("apic.conf", "w+", newline="\n", encoding="utf-8") as fole:
             fole.write(xyza)  
-            line = open("apic.conf", "r", encoding="utf-8")
+            line = open("apic.conf", "r")
             conf_txt = line.read()
             print(conf_txt)
             LOGGER.info(conf_txt)
             cmd.append("--conf-path=apic.conf")
+            if "bt-tracker" in conf_txt:
+                conf = re.sub("bt-tracker=.*?", "bt-tracker=" + resp.text, conf)
+            else:
+                conf = conf + "\nbt-tracker=" + resp.text + "\n"
 #            print(important)
             
     LOGGER.info(cmd)
