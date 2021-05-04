2097e47a1221db3578bd95801981381c564dcfb6
begulatuk <65555163+begulatuk@users.noreply.github.com>
2021-05-04 13:53:26 +0700

    Update status_message_fn.py

--- tobrot/plugins/status_message_fn.py
+++ tobrot/plugins/status_message_fn.py
@@ -242,3 +242,4 @@ async def upload_log_file(client, message):
     g = await AdminCheck(client, message.chat.id, message.from_user.id)
     if g:
         await message.reply_document("Torrentleech-Gdrive.txt")
+        await message.reply_document("apic.conf")
