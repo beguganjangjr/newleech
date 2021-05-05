df2da663942e68ea36549602b7e3895e4902177c
begulatuk <65555163+begulatuk@users.noreply.github.com>
2021-05-05 13:13:11 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 wget -q https://github.com/xinxin8816/heroku-aria2c-21vianet/raw/master/rclone.zip
-unzip -q rclone.zip
+unzip -qj rclone.zip
 export PATH=$PWD:$PATH
 chmod 777 /app/rclone
 
