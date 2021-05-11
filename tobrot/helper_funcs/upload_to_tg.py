4c5c74398360fa2ee533244a12ad8edc9143aec2
begulatuk <65555163+begulatuk@users.noreply.github.com>
2021-05-11 11:36:16 +0700

    fix remove reply text

--- tobrot/helper_funcs/upload_to_tg.py
+++ tobrot/helper_funcs/upload_to_tg.py
@@ -78,12 +78,12 @@ async def upload_to_tg(
         # number_of_files = len(directory_contents)
         LOGGER.info(directory_contents)
         new_m_esg = message
-        #if not message.photo:
-        #    new_m_esg = await message.reply_text(
-        #        f"Found {len(directory_contents)} files <a href='tg://user?id={from_user}'>🤒</a>",
-        #        quote=True
+        if not message.photo:
+            new_m_esg = await message.edit_text(message +=
+                f"\nFound {len(directory_contents)} files <a href='tg://user?id={from_user}'>🤒</a>",
+                quote=True
                 # reply_to_message_id=message.message_id
-        #    )
+            )
         for single_file in directory_contents:
             # recursion: will this FAIL somewhere?
             await upload_to_tg(
